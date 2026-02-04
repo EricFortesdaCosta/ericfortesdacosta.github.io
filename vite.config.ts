e634b30c885111f68b20791e2052d02ea4fc91e3
EricFortesdaCosta <ericcostaf.w@gmail.com>
2026-02-04 13:09:34 -0300

    fixed: page for github

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/portfolioeric.github.io/',
+    base: '/',
 
     server: {
       port: 3000,
